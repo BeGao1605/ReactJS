80d72d25910f2f130ec4b798813455cfca40b1fc
Be Mo <nhanntgc2609@gmail.com>
2022-04-27 23:01:01 +0700

    1010 - State

--- firstapp/src/Xe.js
+++ firstapp/src/Xe.js
@@ -2,11 +2,27 @@ import React from "react";
 
 
 class Xe extends React.Component {
+    constructor(props){
+        console.log('Xe constructor', props.color);
+        super(props);
+        this.state = {
+            color: props.color,
+            value: props.value,
+        }
+    }
+
+
+
     render() {
         console.log("Xe class: ", this.props.color, this.props.value);
-        return <h2 style={{ color: this.props.color }}>
-            Hi, {this.props.value}
-        </h2>
+
+        // this.setState({ color: this.props.color, value: this.props.value });
+
+        return (
+            <h3 style={{ color: this.state.color }}>
+                Hi, {this.state.value}
+            </h3>
+        )
     }
 }
 
